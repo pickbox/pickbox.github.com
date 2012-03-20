ff1691689280715f2dfd72ab2af51c5bbb5de16a
jfojfo@gmail.com <jfojfo@gmail.com@1754ffac-ff99-4cfc-a3db-e2205e847581>
2012-03-20 07:20:20 +0000

    test
    git-svn-id: https://svn.sinaapp.com/httpaddress@7 1754ffac-ff99-4cfc-a3db-e2205e847581

--- 1/index.php
+++ 1/index.php
@@ -1,6 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Origin: http://justlog.sinaapp.com");
-//header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
+header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 readfile("main.html");
 ?>
